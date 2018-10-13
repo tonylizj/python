4c84d309e8a39097314e712e3c92efadf6b8a4dd
Tony Li <tonyli8101@gmail.com>
2018-10-13 13:54:40 -0400

    done

---
+++ CCC/CCC14S3-TheGenevaConfection.py
@@ -0,0 +1,33 @@
+for i in range(int(input())):
+    mountain = []
+    branch = []
+    counter = 1
+    for j in range(int(input())):
+        mountain.append(int(input()))
+
+    while True:
+
+        while branch and branch[-1] == counter:
+            counter += 1
+            branch.pop()
+
+        while mountain and mountain[-1] == counter:
+            counter += 1
+            mountain.pop()
+
+        while mountain and mountain[-1] != counter and (not branch or branch[-1] != counter):
+            branch.append(mountain[-1])
+            mountain.pop()
+
+        if not mountain:
+            while branch and branch[-1] == counter:
+                counter += 1
+                branch.pop()
+
+            if not branch:
+                print("Y")
+                break
+
+            else:
+                print("N")
+                break
